c3122f444a5fd4371cfd9e86467a3023cb570b02
shouguang <495140158@qq.com>
2023-01-10 22:21:14 +0800

    时间格式

--- time/time.go
+++ time/time.go
@@ -5,8 +5,11 @@ import (
 	"time"
 )
 
-// Beauty 好看的时间格式
-const Beauty = "2006-01-02 15:04:05"
+// 时间格式
+const (
+	Date = "2006-01-02"
+	Time = "15:04:05"
+)
 
 // Cost 标准日志库记录函数耗时，在函数顶部调用 defer Cost()()
 func Cost() func() {
