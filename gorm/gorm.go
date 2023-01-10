94a12fba3197de5661191667db02604f7257d67f
shouguang <495140158@qq.com>
2023-01-10 19:31:19 +0800

    注释

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -6,6 +6,9 @@ import (
 	"gorm.io/gorm"
 )
 
+// RecordNotFound 检查查询函数错误是否为 ErrRecordNotFound
+// First, Take, Last 会返回这个错误
+// https://gorm.io/docs/query.html
 func RecordNotFound(e error) bool {
 	return errors.Is(e, gorm.ErrRecordNotFound)
 }
