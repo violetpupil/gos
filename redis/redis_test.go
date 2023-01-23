bef60494eb6c5d6a0ce58b611c4ba994513b6f8b
Shouguang Zhang <495140158@qq.com>
2023-01-23 20:50:12 +0800

    decr & get

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -12,3 +12,17 @@ func TestIncr(t *testing.T) {
 	r, e := Incr(ctx, "tmp")
 	fmt.Print(r, e)
 }
+
+func TestDecr(t *testing.T) {
+	Init("")
+	ctx := context.Background()
+	r, e := Decr(ctx, "tmp")
+	fmt.Print(r, e)
+}
+
+func TestGet(t *testing.T) {
+	Init("")
+	ctx := context.Background()
+	r, e := Get(ctx, "tmp")
+	fmt.Printf("%s %v", r, e)
+}
