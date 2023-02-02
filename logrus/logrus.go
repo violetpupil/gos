bb50869287467f1a14505143c285da7e51755daa
shouguang <495140158@qq.com>
2023-02-02 12:35:15 +0800

    日志函数

--- logrus/logrus.go
+++ logrus/logrus.go
@@ -2,6 +2,14 @@ package logrus
 
 import "github.com/sirupsen/logrus"
 
+var (
+	Info  = logrus.Info
+	Infof = logrus.Infof
+	Error = logrus.Error
+	// Fatal 记录Fatal级别日志，然后退出程序
+	Fatal = logrus.Fatal
+)
+
 func Init() {
 	// 包含调用信息
 	logrus.SetReportCaller(true)
