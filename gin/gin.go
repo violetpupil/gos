483b6991710e750207ddc187e19ebd6c43003df3
Shouguang Zhang <495140158@qq.com>
2023-02-05 22:49:27 +0800

    AdminGroup

--- gin/gin.go
+++ gin/gin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
@@ -10,9 +11,23 @@ import (
 // main 运行http服务在端口8080，直到错误发生
 func main() {
 	e := gin.Default()
-
-	// 注册pprof处理器，提供运行时数据，路径以/debug/pprof/开头
-	pprof.Register(e)
-
+	AdminGroup(e)
 	fmt.Print(e.Run())
+}
+
+// AdminGroup 管理页面
+func AdminGroup(e *gin.Engine) {
+	g := e.Group("/admin", AdminAuth)
+	// 注册pprof处理器，提供运行时数据，路径以/admin/debug/pprof/开头
+	pprof.RouteRegister(g)
+}
+
+// AdminAuth 管理页面认证中间件
+// 使用 qs 参数 ?hypercube=assemble 进行认证
+func AdminAuth(c *gin.Context) {
+	if c.Query("hypercube") != "assemble" {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.Next()
 }
