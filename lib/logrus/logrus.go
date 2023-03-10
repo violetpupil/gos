a564215d1b7031edf25b85ea4adf1238d23de305
shouguang <495140158@qq.com>
2023-03-10 15:55:13 +0800

    日志格式

--- lib/logrus/logrus.go
+++ lib/logrus/logrus.go
@@ -13,4 +13,14 @@ var (
 func Init() {
 	// 包含调用信息
 	logrus.SetReportCaller(true)
+
+	// 日志格式，空日志不显示msg字段
+	// time=2023-03-10T15:43:04+08:00 level=info msg=log
+	logrus.SetFormatter(
+		// 等号形式 - 默认
+		&logrus.TextFormatter{
+			// 值不添加引号
+			DisableQuote: true,
+		},
+	)
 }
