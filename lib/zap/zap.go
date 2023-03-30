5c5af3c6ae1a3459f4bae4d69281296b30fc8a93
shouguang <495140158@qq.com>
2023-03-30 17:37:54 +0800

    zap

---
+++ lib/zap/zap.go
@@ -0,0 +1,35 @@
+// zap好像比logrus块
+package zap
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+var (
+	// 全局logger
+	L = zap.L
+)
+
+// InitDevelopment 将zap全局logger设置为开发
+func InitDevelopment() error {
+	l, err := zap.NewDevelopment()
+	if err != nil {
+		log.Println("new development error", err)
+		return err
+	}
+	zap.ReplaceGlobals(l)
+	return nil
+}
+
+// InitProduction 将zap全局logger设置为生产
+func InitProduction() error {
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Println("new production error", err)
+		return err
+	}
+	zap.ReplaceGlobals(l)
+	return nil
+}
