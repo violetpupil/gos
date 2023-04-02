f13fd91f66917ade2da4577ee1fec238b73feec2
Shouguang Zhang <495140158@qq.com>
2023-04-02 23:43:59 +0800

    Find

--- comm/gorm/query.go
+++ comm/gorm/query.go
@@ -10,11 +10,60 @@ import (
 )
 
 // RecordNotFound 检查查询函数错误是否为 ErrRecordNotFound
-// First, Take, Last 会返回这个错误
 func RecordNotFound(e error) bool {
 	return errors.Is(e, gorm.ErrRecordNotFound)
 }
 
 type query struct {
 	db *gorm.DB
+}
+
+// First 查找根据主键排序的第一条数据，无主键则根据第一个字段排序
+// dest是数据模型指针，返回bool表示是否找到
+// SELECT * FROM users ORDER BY id LIMIT 1;
+func (q *query) First(dest interface{}, cond ...interface{}) (bool, error) {
+	err := q.db.First(dest, cond...).Error
+	if RecordNotFound(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
+
+// Last 查找根据主键排序的最后一条数据，无主键则根据第一个字段排序
+// dest是数据模型指针，返回bool表示是否找到
+// SELECT * FROM users ORDER BY id DESC LIMIT 1;
+func (q *query) Last(dest interface{}, cond ...interface{}) (bool, error) {
+	err := q.db.Last(dest, cond...).Error
+	if RecordNotFound(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
+
+// Take 查找第一条数据
+// dest是数据模型指针，返回bool表示是否找到
+// SELECT * FROM users LIMIT 1;
+func (q *query) Take(dest interface{}, cond ...interface{}) (bool, error) {
+	err := q.db.Take(dest, cond...).Error
+	if RecordNotFound(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
+
+// Find 查询数据
+// dest是数据模型切片指针，返回找到的记录数
+// SELECT * FROM users;
+func (q *query) Find(dest interface{}, cond ...interface{}) (int64, error) {
+	db := q.db.Find(dest, cond...)
+	return db.RowsAffected, db.Error
 }
