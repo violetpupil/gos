0bd4f0f1b3e5cc80830b6677c4145d8ae98b1822
shouguang <495140158@qq.com>
2023-04-03 14:41:01 +0800

    Path

---
+++ std/url/url.go
@@ -0,0 +1,21 @@
+package url
+
+import (
+	"net/url"
+	"strings"
+
+	"github.com/violetpupil/components/lib/logrus"
+)
+
+// Path 返回url的路径部分，无前缀斜杆
+func Path(rawURL string) (string, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		logrus.Errorln("url parse error", err)
+		return "", err
+	}
+	// 参数是不以斜杆开头的路径时，u.Path 也没有前置斜杆
+	// 其它情况 u.Path 会以斜杆开头
+	path := strings.TrimPrefix(u.Path, "/")
+	return path, nil
+}
---
+++ std/url/url_test.go
@@ -0,0 +1,10 @@
+package url
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPath(t *testing.T) {
+	fmt.Println(Path("https://pkg.go.dev/net/url"))
+}
