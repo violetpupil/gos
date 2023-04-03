1e3a8a7b683b147dceac1fad39087928421b07de
shouguang <495140158@qq.com>
2023-04-03 11:47:39 +0800

    主键查询

--- comm/gorm/query.go
+++ comm/gorm/query.go
@@ -21,6 +21,20 @@ type query struct {
 // First 查找根据主键排序的第一条数据，无主键则根据第一个字段排序
 // dest是数据模型指针，返回bool表示是否找到
 // SELECT * FROM users ORDER BY id LIMIT 1;
+//
+// 当dest主键有值
+// SELECT * FROM users WHERE id = 10;
+// var user = User{ID: 10}
+// db.First(&user)
+//
+// 当主键是数字
+// SELECT * FROM users WHERE id = 10;
+// db.First(&user, 10)
+// db.First(&user, "10") 小心sql注入
+//
+// 主键不限定类型
+// SELECT * FROM users WHERE id = "1b74413f-f3b8-409f-ac47-e8c062e3472a";
+// db.First(&user, "id = ?", "1b74413f-f3b8-409f-ac47-e8c062e3472a")
 func (q *query) First(dest interface{}, cond ...interface{}) (bool, error) {
 	err := q.db.First(dest, cond...).Error
 	if RecordNotFound(err) {
@@ -63,6 +77,10 @@ func (q *query) Take(dest interface{}, cond ...interface{}) (bool, error) {
 // Find 查询数据
 // dest是数据模型切片指针，返回找到的记录数
 // SELECT * FROM users;
+//
+// 当主键是数字
+// SELECT * FROM users WHERE id IN (1,2,3);
+// db.Find(&users, []int{1,2,3})
 func (q *query) Find(dest interface{}, cond ...interface{}) (int64, error) {
 	db := q.db.Find(dest, cond...)
 	return db.RowsAffected, db.Error
