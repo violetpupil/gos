037f2221b0458b8ef4cfa9508358d217e5017965
shouguang <495140158@qq.com>
2023-04-17 14:10:05 +0800

    FindAllStringSubmatch

--- lib/regexp/regexp.go
+++ lib/regexp/regexp.go
@@ -18,3 +18,14 @@ func FindAllString(expr, s string) ([]string, error) {
 	res := r.FindAllString(s, -1)
 	return res, nil
 }
+
+// FindAllStringSubmatch 查找所有匹配的字符串 - 子匹配
+func FindAllStringSubmatch(expr, s string) ([][]string, error) {
+	r, err := regexp.Compile(expr)
+	if err != nil {
+		logrus.Errorln("regexp compile error", err)
+		return nil, err
+	}
+	res := r.FindAllStringSubmatch(s, -1)
+	return res, err
+}
--- lib/regexp/regexp_test.go
+++ lib/regexp/regexp_test.go
@@ -9,3 +9,8 @@ func TestFindAllString(t *testing.T) {
 	r, e := FindAllString("a", "abc")
 	fmt.Println(r, e)
 }
+
+func TestFindAllStringSubmatch(t *testing.T) {
+	r, e := FindAllStringSubmatch("a(b)", "abc")
+	fmt.Println(r, e)
+}
