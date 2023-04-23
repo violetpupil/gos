7ad2c58321c402c204f69d917c0247caf8396796
Shouguang Zhang <495140158@qq.com>
2023-04-24 00:04:49 +0800

    集群内客户端

---
+++ lib/k8s/examples/main.go
@@ -0,0 +1,39 @@
+// 集群内客户端
+// https://github.com/kubernetes/client-go/tree/master/examples/in-cluster-client-configuration
+//
+// 编译
+// set GOOS=linux
+// go build -o ./app .
+//
+// Dockerfile
+// FROM debian
+// COPY ./app /app
+// ENTRYPOINT /app
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/violetpupil/components/lib/k8s"
+)
+
+func main() {
+	err := k8s.NewInCluster()
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	for {
+		pods, err := k8s.PodsList(ctx)
+		if err != nil {
+			logrus.Errorln("pods list error", err)
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		time.Sleep(10 * time.Second)
+	}
+}
