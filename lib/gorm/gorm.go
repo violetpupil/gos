c39017be17307a02dfe7c4099818445dadda7d1e
shouguang <495140158@qq.com>
2023-04-24 15:00:56 +0800

    DisableForeignKeyConstraintWhenMigrating

--- lib/gorm/gorm.go
+++ lib/gorm/gorm.go
@@ -4,11 +4,12 @@ package gorm
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/caarlos0/env/v7"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // crud 数据库操作
@@ -34,15 +35,34 @@ func Init(db *gorm.DB) {
 	Crud.Raw = &raw{db}
 }
 
+// Config mysql配置
+type Config struct {
+	Host     string `env:"MySQLHost"`
+	Port     string `env:"MySQLPort"`
+	User     string `env:"MySQLUser"`
+	Pass     string `env:"MySQLPass"`
+	Database string `env:"MySQLDatabase"`
+	// gorm.Config
+	Logger logger.Interface
+	// 迁移模型时，不创建外键约束
+	DisableForeignKeyConstraintWhenMigrating bool
+}
+
 // InitMySQL 初始化mysql数据库操作
 // https://gorm.io/docs/connecting_to_the_database.html
 // https://github.com/go-sql-driver/mysql
-func InitMySQL(user, pass, host, port, d string) error {
+func InitMySQL(c Config) error {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		user, pass, host, port, d,
+		c.User, c.Pass, c.Host, c.Port, c.Database,
 	)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dialector := mysql.Open(dsn)
+
+	opt := &gorm.Config{
+		Logger:                                   c.Logger,
+		DisableForeignKeyConstraintWhenMigrating: c.DisableForeignKeyConstraintWhenMigrating,
+	}
+	db, err := gorm.Open(dialector, opt)
 	if err != nil {
 		logrus.Errorln("open mysql error", err)
 		return err
@@ -51,28 +71,22 @@ func InitMySQL(user, pass, host, port, d string) error {
 	return nil
 }
 
-const (
-	EnvHost     = "MySQLHost"
-	EnvPort     = "MySQLPort"
-	EnvUser     = "MySQLUser"
-	EnvPass     = "MySQLPass"
-	EnvDatabase = "MySQLDatabase"
-)
-
 // InitMySQLEnv 使用环境变量初始化mysql数据库操作
 func InitMySQLEnv() error {
-	host := os.Getenv(EnvHost)
-	port := os.Getenv(EnvPort)
-	user := os.Getenv(EnvUser)
-	pass := os.Getenv(EnvPass)
-	db := os.Getenv(EnvDatabase)
-	err := InitMySQL(user, pass, host, port, db)
+	var c Config
+	err := env.Parse(&c)
+	if err != nil {
+		logrus.Errorln("env parse error", err)
+		return err
+	}
+	err = InitMySQL(c)
 	return err
 }
 
 // AutoMigrate 自动迁移模型，不会删除列
 // https://gorm.io/docs/migration.html
 // dst是数据模型指针
+// 默认创建数据库外键约束
 func (c *crud) AutoMigrate(dst ...any) error {
 	err := c.db.AutoMigrate(dst...)
 	return err
