bbad8f6297eede475979cd56984313654a280772
shouguang <495140158@qq.com>
2023-04-25 15:21:56 +0800

    prometheus

---
+++ lib/prometheus/prometheus.go
@@ -0,0 +1,3 @@
+// https://prometheus.io/docs/guides/go-application/
+// https://github.com/prometheus/client_golang/tree/main/examples
+package prometheus
---
+++ tools/prometheus.md
@@ -0,0 +1,3 @@
+# [prometheus](https://prometheus.io/)
+
+[文档](https://prometheus.io/docs/introduction/overview/)
