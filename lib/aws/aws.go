c71b533e7ae49d126565c79f469f5661cf775623
shouguang <495140158@qq.com>
2023-04-25 17:17:06 +0800

    aws

---
+++ lib/aws/aws.go
@@ -0,0 +1,3 @@
+// https://docs.aws.amazon.com/sdk-for-go/
+// https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/gov2
+package aws
---
+++ lib/aws/s3/s3.go
@@ -0,0 +1,3 @@
+// https://aws.amazon.com/cn/s3/
+// https://docs.aws.amazon.com/s3/
+package s3
