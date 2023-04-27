9dcaa70fb06cfa747781c0ffa8bb7a8c4ffc0cad
shouguang <495140158@qq.com>
2023-04-27 22:38:03 +0800

    Send

--- api/ding/ding.go
+++ api/ding/ding.go
@@ -3,11 +3,31 @@
 // https://open.dingtalk.com/document/orgapp/robot-overview
 package ding
 
-const SendURL = "https://oapi.dingtalk.com/robot/send"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/violetpupil/components/lib/resty"
+)
 
 // Send 发送群消息
 // 每个机器人每分钟最多发送20条消息到群里，如果超过20条，会限流10分钟。
 // https://open.dingtalk.com/document/orgapp/custom-robots-send-group-messages
-func Send(token string, text string) error {
+func Send(token string, body any) error {
+	res, err := resty.Post(
+		"https://oapi.dingtalk.com/robot/send",
+		func(r *resty.Request) *resty.Request {
+			r.SetQueryParam("access_token", token)
+			r.SetBody(body)
+			return r
+		},
+	)
+	if err != nil {
+		logrus.Errorln("post error", err)
+		return err
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"Status": res.Status(),
+		"Body":   res.String(),
+	}).Info("send response")
 	return nil
 }
