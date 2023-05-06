67708977cda9356b98d042b864e374811f720b18
Shouguang Zhang <495140158@qq.com>
2023-05-06 20:40:49 +0800

    ResponseWriter

---
+++ std/http/res.go
@@ -0,0 +1,10 @@
+package http
+
+import "net/http"
+
+// ResponseWriter 构造http响应
+// WriteHeader() 写响应行和响应头
+// Write() 写响应体
+// 直接调用Write()，会自动调用WriteHeader()，使用200响应码
+// 如果响应头没有Content-Type，Write()会自动检测并设置
+type ResponseWriter = http.ResponseWriter
