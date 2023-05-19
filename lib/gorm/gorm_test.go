e33453103b47248411ec89e4eca02b7d25fb692c
shouguang <495140158@qq.com>
2023-05-19 14:30:23 +0800

    datatypes.JSON

--- lib/gorm/gorm_test.go
+++ lib/gorm/gorm_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/violetpupil/components/lib/godotenv"
+	"gorm.io/datatypes"
 )
 
 func TestInitMySQLEnv(t *testing.T) {
@@ -18,6 +19,7 @@ type Tmp struct {
 	Age    int    `gorm:"uniqueIndex:idx_age_name"`
 	Name   string `gorm:"uniqueIndex:idx_age_name;type:varchar(10)"`
 	Beauty bool
+	Extend datatypes.JSON
 }
 
 func Test_crud_AutoMigrate(t *testing.T) {
