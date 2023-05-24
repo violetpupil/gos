214cddbf7c763f248db3ab80e6552a236956be60
shouguang <495140158@qq.com>
2023-05-24 11:21:28 +0800

    EtcdConf

--- lib/zero/service.go
+++ lib/zero/service.go
@@ -4,5 +4,6 @@ import "github.com/zeromicro/go-zero/core/service"
 
 type (
 	// ServiceConf is a service config.
+	// Name service name
 	ServiceConf = service.ServiceConf
 )
--- lib/zero/zrpc.go
+++ lib/zero/zrpc.go
@@ -4,5 +4,7 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type (
 	// RpcServerConf is a rpc server config.
+	// ListenOn 监听地址
+	// Etcd etcd related configuration
 	RpcServerConf = zrpc.RpcServerConf
 )
