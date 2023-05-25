b973777d0c09f4aa8c44893b1664507be2e01823
shouguang <495140158@qq.com>
2023-05-25 16:20:27 +0800

    protojson

---
+++ lib/protobuf/protojson.go
@@ -0,0 +1,15 @@
+package protobuf
+
+// This package produces a different output than the standard "encoding/json" package,
+// which does not operate correctly on protocol buffer messages.
+import "google.golang.org/protobuf/encoding/protojson"
+
+var (
+	// Format formats the message as a multiline string.
+	// This function is only intended for human consumption and ignores errors.
+	Format = protojson.Format
+	// Marshal json编码
+	Marshal = protojson.Format
+	// Unmarshal json解码
+	Unmarshal = protojson.Format
+)
