a32d562cf0e6b748ad595392e9727efb7282ccc2
shouguang <495140158@qq.com>
2023-05-26 16:10:24 +0800

    Builder

--- std/fmt/fmt.go
+++ std/fmt/fmt.go
@@ -1,2 +1,9 @@
 // 实现 error 或 fmt.Stringer 接口，会使 %+v 失效
 package fmt
+
+import "fmt"
+
+var (
+	// 写io.Writer
+	Fprintf = fmt.Fprintf
+)
---
+++ std/strings/strings.go
@@ -0,0 +1,11 @@
+package strings
+
+import "strings"
+
+type (
+	// 字符串构造器
+	// Write() 写字节数组
+	// WriteByte() 写单个字节
+	// WriteString() 写字符串
+	Builder = strings.Builder
+)
