53509f6fc5e369ee02329840e869231ae9c15468
shouguang <495140158@qq.com>
2023-05-26 15:54:44 +0800

    RawScan

--- lib/gorm/raw.go
+++ lib/gorm/raw.go
@@ -7,8 +7,14 @@ type raw struct {
 	db *gorm.DB
 }
 
-// Exec 直接执行sql语句
+// Exec 直接执行sql语句，不能获取查询结果
 func (r *raw) Exec(sql string, values ...interface{}) error {
 	err := r.db.Exec(sql, values...).Error
+	return err
+}
+
+// RawScan 直接执行sql语句，并获取结果
+func (r *raw) RawScan(sql string, dest interface{}, values ...interface{}) error {
+	err := r.db.Raw(sql, values...).Scan(dest).Error
 	return err
 }
