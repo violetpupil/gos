0e7229c0f2550708d37f2cdfc69d1362d8149339
shouguang <495140158@qq.com>
2023-05-26 09:43:10 +0800

    SendClient

--- api/ding/ding.go
+++ api/ding/ding.go
@@ -24,3 +24,18 @@ func Send(token string, body any) {
 		logrus.Infoln("send ding response", res.String())
 	}
 }
+
+// SendClient 发送群消息，新创建resty客户端
+// 每个机器人每分钟最多发送20条消息到群里，如果超过20条，会限流10分钟。
+// https://open.dingtalk.com/document/orgapp/custom-robots-send-group-messages
+func SendClient(token string, body any) {
+	req := resty.New().R()
+	req.SetQueryParam("access_token", token)
+	req.SetBody(body)
+	res, err := req.Post(SendURL)
+	if err != nil {
+		logrus.Errorln("send ding error", err)
+	} else {
+		logrus.Infoln("send ding response", res.String())
+	}
+}
--- lib/resty/resty.go
+++ lib/resty/resty.go
@@ -8,6 +8,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	// New method creates a new Resty client.
+	New = resty.New
+)
+
 // Client http客户端
 var Client *resty.Client
 
