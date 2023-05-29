a86ac1c4866012ee7fc270a11ac726ad6a9b7e6d
shouguang <495140158@qq.com>
2023-05-29 17:58:20 +0800

    Logger

---
+++ lib/zero/logx.go
@@ -0,0 +1,12 @@
+package zero
+
+import "github.com/zeromicro/go-zero/core/logx"
+
+type (
+	// Logger 日志记录
+	// Error() fmt.Sprint
+	// Errorf() fmt.Sprintf
+	// Errorv() json编码后记录
+	// Errorw() 记录键值对
+	Logger = logx.Logger
+)
