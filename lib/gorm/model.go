b7c81310e5e0a5a1aaf29581fb32487056578d33
shouguang <495140158@qq.com>
2023-05-30 12:51:08 +0800

    Model

--- lib/gorm/model.go
+++ lib/gorm/model.go
@@ -2,17 +2,21 @@
 // By default, GORM uses ID as primary key,
 // pluralizes struct name to snake_cases as table name, snake_case as column name,
 // and uses CreatedAt, UpdatedAt to track creating/updating time
+// 使用TableName()方法自定义表名
+//
+// 模型tag `gorm:""`
+// 键值用:连接，字段用;连接
+// column 列名
+// comment 注释
 package gorm
 
 import "gorm.io/gorm"
 
 type (
 	// 基础模型
-	// 使用TableName()方法自定义表名
-	//
-	// 模型tag `gorm:""`
-	// 键值用:连接，字段用;连接
-	// column 列名
-	// comment 注释
+	// id bigint
+	// created_at datetime
+	// updated_at datetime
+	// deleted_at datetime
 	Model = gorm.Model
 )
