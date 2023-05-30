b65f13e287b7344f97419aee721ee09b9a17fbf6
shouguang <495140158@qq.com>
2023-05-30 18:10:55 +0800

    ZRange

--- lib/redis/delay/delay.go
+++ lib/redis/delay/delay.go
@@ -1,5 +1,53 @@
 // redis延迟队列
-// 使用list存放到期的任务id
-// 使用sorted set存放未到期的任务id
 // https://github.com/ouqiang/delay-queue
 package delay
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+const (
+	// 使用list存放到期的任务id
+	LName = "dq_queue"
+	// 使用sorted set存放未到期的任务id
+	ZName = "dq_bucket"
+)
+
+var client *redis.Client
+
+// Init 初始化客户端
+// 初始化时不会连接 redis
+func Init(addr, pass string) {
+	opt := &redis.Options{
+		Addr:     addr,
+		Password: pass,
+	}
+	client = redis.NewClient(opt)
+	// 每秒查询一次是否到期
+	time.AfterFunc(time.Second, Move)
+}
+
+// ZAdd 设置任务延迟，id为任务标识
+func ZAdd(d time.Duration, id string) error {
+	ctx := context.Background()
+	ts := time.Now().Add(d).Unix()
+	z := &redis.Z{Score: float64(ts), Member: id}
+	err := client.ZAdd(ctx, ZName, z).Err()
+	return err
+}
+
+// ZRange 获取到期的任务
+func ZRange() ([]string, error) {
+	ctx := context.Background()
+	ts := time.Now().Unix()
+	by := &redis.ZRangeBy{Min: "-inf", Max: fmt.Sprintf("%d", ts)}
+	ids, err := client.ZRangeByScore(ctx, ZName, by).Result()
+	return ids, err
+}
+
+// Move 将到期的任务id转移到list
+func Move() {}
--- lib/redis/sorted_set.go
+++ lib/redis/sorted_set.go
@@ -1,1 +1,28 @@
 package redis
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// ZRange 获取有序集合元素
+// start stop 前后包含
+// zrange key 0 -1 获取全部元素
+// https://redis.io/commands/zrange/
+func ZRange(ctx context.Context, key string, start, stop int64) ([]string, error) {
+	return client.ZRange(ctx, key, start, stop).Result()
+}
+
+// ZRangeWithScores 获取有序集合元素 包含得分
+func ZRangeWithScores(ctx context.Context, key string, start, stop int64) ([]redis.Z, error) {
+	return client.ZRangeWithScores(ctx, key, start, stop).Result()
+}
+
+// ZRangeByScore 获取有序集合元素 根据得分
+// start stop 前后包含
+// zrange key -inf +inf byScore 获取全部元素
+func ZRangeByScore(ctx context.Context, key string, start, stop string) ([]string, error) {
+	by := &redis.ZRangeBy{Min: start, Max: stop}
+	return client.ZRangeByScore(ctx, key, by).Result()
+}
