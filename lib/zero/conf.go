a8a304258698b55436c001db36355079c206a2ab
shouguang <495140158@qq.com>
2023-05-30 12:45:24 +0800

    MustLoad

--- lib/zero/conf.go
+++ lib/zero/conf.go
@@ -1,15 +1,16 @@
 // 配置
 // 直接从环境变量读取 `json:",env=SERVICE_NAME"`
-// 或者yaml文件读取环境变量 ${SERVICE_NAME}
+// 或者配置yaml文件 ${SERVICE_NAME}
 package zero
 
 import "github.com/zeromicro/go-zero/core/conf"
 
 var (
-	// 加载配置，失败则退出程序
-	MustLoad = conf.MustLoad
 	// 加载配置，失败则返回错误
 	Load = conf.Load
-	// 使用环境变量
-	UseEnv = conf.UseEnv
 )
+
+// MustLoad 加载配置，失败则退出程序
+func MustLoad(path string, v any) {
+	conf.MustLoad(path, v, conf.UseEnv())
+}
