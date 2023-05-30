919ef7b9a515f5f88c08873839256d64c535a48c
shouguang <495140158@qq.com>
2023-05-30 21:39:20 +0800

    RedisConf

---
+++ lib/zero/redis.go
@@ -0,0 +1,10 @@
+package zero
+
+import "github.com/zeromicro/go-zero/core/stores/redis"
+
+type (
+	// A RedisConf is a redis config.
+	// Host redis地址 localhost:6379
+	// Pass redis密码
+	RedisConf = redis.RedisConf
+)
