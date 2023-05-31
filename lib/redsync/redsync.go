c9043e688b7eb07f0f603b2ef819095fe7791abc
shouguang <495140158@qq.com>
2023-05-31 21:33:46 +0800

    New

--- lib/redsync/redsync.go
+++ lib/redsync/redsync.go
@@ -9,12 +9,20 @@ import (
 )
 
 // m redis分布式锁管理
+// 必须先调用Init初始化
 var m *redsync.Redsync
 
 // Init 初始化锁管理
 func Init(client *redis.Client) {
 	pool := goredis.NewPool(client)
 	m = redsync.New(pool)
+}
+
+// New 创建redis分布式锁管理
+func New(client *redis.Client) *redsync.Redsync {
+	pool := goredis.NewPool(client)
+	m := redsync.New(pool)
+	return m
 }
 
 // NewMutex returns a new distributed mutex with given name.
