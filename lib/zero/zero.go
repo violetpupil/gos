a7e755a19aad68ba61fb63606d7e9533c2f29ceb
shouguang <495140158@qq.com>
2023-05-31 09:39:31 +0800

    Config

--- lib/zero/zero.go
+++ lib/zero/zero.go
@@ -3,3 +3,24 @@
 // https://go-zero.dev/cn/docs/blog/blog
 // https://github.com/zeromicro/zero-examples
 package zero
+
+import "github.com/zeromicro/go-zero/zrpc"
+
+// Config rpc服务配置
+type Config struct {
+	zrpc.RpcServerConf
+	MySQL struct {
+		Host     string
+		Port     int
+		User     string
+		Pass     string
+		Database string
+	}
+	// zrpc.RpcServerConf.Redis字段必须设置Key
+	// 所以另外定义字段，且字段名不能和Redis重名
+	RedisC struct {
+		Host string
+		Port int
+		Pass string
+	}
+}
