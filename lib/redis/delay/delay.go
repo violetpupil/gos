31e3020a86d344f5eb566e8ed063c5df5027b8cb
shouguang <495140158@qq.com>
2023-06-01 16:04:42 +0800

    RPush

--- lib/redis/delay/delay.go
+++ lib/redis/delay/delay.go
@@ -12,67 +12,74 @@ import (
 	"github.com/violetpupil/components/spec/types"
 )
 
-const (
-	// 使用list存放到期的任务id
-	lName = "dq_queue"
-	// 使用sorted set存放未到期的任务id
-	zName = "dq_bucket"
-)
+// Delay redis延迟队列
+type Delay struct {
+	client *redis.Client
+	// redis list 存放到期的任务id
+	lName string
+	// redis sorted set 存放未到期的任务id
+	zName string
+}
 
-var client *redis.Client
-
-// Init 初始化redis客户端
+// NewDelay 创建Delay实例
 // 启动goroutine，使用f处理到期的任务，参数为任务id数组
-func Init(addr, pass string, f func([]string)) {
-	opt := &redis.Options{
-		Addr:     addr,
-		Password: pass,
+//
+// name用于组成redis键名
+// list 存放到期的任务id，键名为dq_queue:name
+// sorted set 存放未到期的任务id，键名为dq_bucket:name
+// 每次创建Delay实例时，有序集合键名应该在redis库空间中唯一
+// 否则，可能导致list中的任务id重复
+func NewDelay(r *redis.Client, name string, f func([]string)) *Delay {
+	d := &Delay{
+		client: r,
+		lName:  "dq_queue:" + name,
+		zName:  "dq_bucket:" + name,
 	}
-	client = redis.NewClient(opt)
 	// 每秒查询一次是否到期
-	time.AfterFunc(time.Second, move)
-	go bLPop(f)
+	time.AfterFunc(time.Second, d.move)
+	go d.bLPop(f)
+	return d
 }
 
 // ZAdd 设置任务延迟，id为任务标识
-func ZAdd(d time.Duration, id string) error {
+func (d *Delay) ZAdd(dur time.Duration, id string) error {
 	ctx := context.Background()
-	ts := time.Now().Add(d).Unix()
+	ts := time.Now().Add(dur).Unix()
 	z := redis.Z{Score: float64(ts), Member: id}
-	err := client.ZAdd(ctx, zName, z).Err()
+	err := d.client.ZAdd(ctx, d.zName, z).Err()
 	return err
 }
 
-// ZRem 移除任务检测
-func ZRem(ids []string) error {
+// ZRem 移除任务到期检测
+func (d *Delay) ZRem(ids []string) error {
 	ctx := context.Background()
-	err := client.ZRem(ctx, zName, ids).Err()
+	err := d.client.ZRem(ctx, d.zName, ids).Err()
+	return err
+}
+
+// RPush 将任务id放到到期队列
+func (d *Delay) RPush(ids []string) error {
+	ctx := context.Background()
+	idsI := types.ToSlice(ids)
+	err := d.client.RPush(ctx, d.lName, idsI...).Err()
 	return err
 }
 
 // zRange 到期任务检测
-func zRange() ([]string, error) {
+func (d *Delay) zRange() ([]string, error) {
 	ctx := context.Background()
 	ts := time.Now().Unix()
 	by := &redis.ZRangeBy{Min: "-inf", Max: fmt.Sprintf("%d", ts)}
-	ids, err := client.ZRangeByScore(ctx, zName, by).Result()
+	ids, err := d.client.ZRangeByScore(ctx, d.zName, by).Result()
 	return ids, err
-}
-
-// rPush 将任务id放到到期队列
-func rPush(ids []string) error {
-	ctx := context.Background()
-	idsI := types.ToSlice(ids)
-	err := client.RPush(ctx, lName, idsI...).Err()
-	return err
 }
 
 // bLPop 处理到期的任务，f参数为任务id数组
 // 该函数会阻塞
-func bLPop(f func([]string)) {
+func (d *Delay) bLPop(f func([]string)) {
 	for {
 		ctx := context.Background()
-		ids, err := client.BLPop(ctx, 0, lName).Result()
+		ids, err := d.client.BLPop(ctx, 0, d.lName).Result()
 		if err != nil {
 			logrus.Errorln("block left pop error", err)
 			time.Sleep(10 * time.Second)
@@ -83,9 +90,8 @@ func bLPop(f func([]string)) {
 }
 
 // move 将到期的任务id转移到list
-// 多进程运行，list中的任务id可能有重复
-func move() {
-	ids, err := zRange()
+func (d *Delay) move() {
+	ids, err := d.zRange()
 	if err != nil {
 		logrus.Errorln("zrange expire error", err)
 		return
@@ -95,12 +101,12 @@ func move() {
 		return
 	}
 
-	err = rPush(ids)
+	err = d.RPush(ids)
 	if err != nil {
 		logrus.Errorln("push expire error", err)
 		return
 	}
-	err = ZRem(ids)
+	err = d.ZRem(ids)
 	if err != nil {
 		logrus.Errorln("rem expire error", err)
 	}
