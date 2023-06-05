d320224382628187756a98e5b913fe8d4e2c4c88
shouguang <495140158@qq.com>
2023-06-05 17:10:26 +0800

    Transaction

--- lib/gorm/transactions.go
+++ lib/gorm/transactions.go
@@ -12,6 +12,8 @@ type transaction struct {
 
 // Transaction 在事务中调用fc
 // fc返回错误则回滚，否则提交
-func (t *transaction) Transaction(fc func(tx *DB) error, opts ...*sql.TxOptions) error {
+// 注意fc中使用参数tx执行操作
+// fc返回的错误会传递出来
+func (t *transaction) Transaction(fc func(tx *gorm.DB) error, opts ...*sql.TxOptions) error {
 	return t.db.Transaction(fc, opts...)
 }
