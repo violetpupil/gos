db29f109d311bfe4ac42431a7e6b1bf5fa776095
Shouguang Zhang <495140158@qq.com>
2023-06-06 23:04:41 +0800

    SetNx

--- lib/redis/string.go
+++ lib/redis/string.go
@@ -1,6 +1,9 @@
 package redis
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 // Incr 将指定键的值加1，返回增加后的值
 // 键不存在的话，先创建键并设置为0，后执行操作
@@ -17,8 +20,20 @@ func Decr(ctx context.Context, key string) (int64, error) {
 }
 
 // Get 获取字符串键值
-// 键不存在的话，返回 Nil 错误
+// 键不存在的话，返回 redis.Nil 错误
 // https://redis.io/commands/get/
 func Get(ctx context.Context, key string) (string, error) {
 	return client.Get(ctx, key).Result()
+}
+
+// Set 设置字符串
+// https://redis.io/commands/set/
+func Set(ctx context.Context, key string, value interface{}, e time.Duration) error {
+	return client.Set(ctx, key, value, e).Err()
+}
+
+// SetNx 设置字符串 如果不存在
+// 字符串存在返回redis.Nil
+func SetNx(ctx context.Context, key string, value interface{}, e time.Duration) error {
+	return client.SetEx(ctx, key, value, e).Err()
 }
