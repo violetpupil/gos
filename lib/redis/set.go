7dd66d1775f59afe7148db225ff08a072ef4e8d8
shouguang <495140158@qq.com>
2023-06-07 11:11:40 +0800

    SRem

--- lib/redis/set.go
+++ lib/redis/set.go
@@ -8,3 +8,10 @@ import "context"
 func SAdd(ctx context.Context, key string, members ...interface{}) (int64, error) {
 	return client.SAdd(ctx, key, members...).Result()
 }
+
+// SRem set rem
+// 返回移除成功的元素数
+// https://redis.io/commands/srem/
+func SRem(ctx context.Context, key string, members ...interface{}) (int64, error) {
+	return client.SRem(ctx, key, members...).Result()
+}
