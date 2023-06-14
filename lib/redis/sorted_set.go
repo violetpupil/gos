8e16227deb973da1d60a812e449d8db2d9113b19
shouguang <495140158@qq.com>
2023-06-14 14:53:30 +0800

    ZRemRangeByScore

--- lib/redis/sorted_set.go
+++ lib/redis/sorted_set.go
@@ -8,6 +8,22 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// ZAdd 添加元素
+// 如果元素存在，更新得分
+// 返回添加的元素数，不包括更新
+// https://redis.io/commands/zadd/
+func ZAdd(ctx context.Context, key string, members ...redis.Z) (int64, error) {
+	return client.ZAdd(ctx, key, members...).Result()
+}
+
+// ZAddNX 添加元素
+// 如果元素存在，不更新得分
+// 返回添加的元素数
+// https://redis.io/commands/zadd/
+func ZAddNX(ctx context.Context, key string, members ...redis.Z) (int64, error) {
+	return client.ZAddNX(ctx, key, members...).Result()
+}
+
 // ZRange 获取有序集合元素 根据索引
 // start stop 前后包含
 // zrange key 0 -1 获取全部元素
@@ -34,5 +50,12 @@ func ZRangeByScore(ctx context.Context, key string, start, stop string) ([]strin
 // https://redis.io/commands/zrem/
 func ZRem(ctx context.Context, key string, members ...interface{}) error {
 	err := client.ZRem(ctx, key, members...).Err()
+	return err
+}
+
+// ZRemRangeByScore 移除元素 根据得分
+// https://redis.io/commands/zremrangebyscore/
+func ZRemRangeByScore(ctx context.Context, key, min, max string) error {
+	err := client.ZRemRangeByScore(ctx, key, min, max).Err()
 	return err
 }
