2737348e31bc72a4d2067e83891fba0846d1a40e
Shouguang Zhang <495140158@qq.com>
2023-06-16 00:27:12 +0800

    gen

---
+++ lib/gorm/gen.go
@@ -0,0 +1,9 @@
+// https://gorm.io/gen/
+// https://gorm.io/gen/gen_tool.html
+// go install gorm.io/gen/tools/gentool@latest
+// -dsn data source name
+// -modelPkgName generated model code's package name
+// -outPath specify a directory for output (default "./dao/query")
+// -tables specify tables want to generated from, default all tables. 逗号分隔
+// -onlyModel only generate models (without query file)
+package gorm
