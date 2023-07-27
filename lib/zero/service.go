5d80b115407b5e3aed18110d741205f08515917f
shouguang <495140158@qq.com>
2023-07-27 10:18:26 +0800

    运行环境

--- lib/zero/service.go
+++ lib/zero/service.go
@@ -2,8 +2,18 @@ package zero
 
 import "github.com/zeromicro/go-zero/core/service"
 
+// 运行环境
+const (
+	DevMode  = "dev"  // 开发
+	TestMode = "test" // 测试
+	RtMode   = "rt"   // 回归测试
+	PreMode  = "pre"  // 预生产
+	ProMode  = "pro"  // 生产
+)
+
 type (
 	// ServiceConf is a service config.
 	// Name service name
+	// Mode 运行环境，默认为生产环境
 	ServiceConf = service.ServiceConf
 )
