39784f9ef67804a14ab76706b5020d0718334f26
shouguang <495140158@qq.com>
2023-08-02 16:02:15 +0800

    平衡二叉树

--- ald/search_tree.go
+++ ald/search_tree.go
@@ -4,6 +4,8 @@
 // https://en.wikipedia.org/wiki/Search_tree
 // 二叉查找树 父节点大于左子树的所有节点，小于右子树的所有节点
 // https://en.wikipedia.org/wiki/Binary_search_tree
+// 平衡二叉树 使二叉查找树平衡
+// https://en.wikipedia.org/wiki/Self-balancing_binary_search_tree
 // B树
 // https://en.wikipedia.org/wiki/B-tree
 // B+树
