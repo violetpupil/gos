f11150f00bab2752640b1493b0226c74455ad098
shouguang <495140158@qq.com>
2023-08-02 15:48:23 +0800

    二叉查找树

--- ald/search_tree.go
+++ ald/search_tree.go
@@ -2,6 +2,8 @@
 // https://en.wikipedia.org/wiki/Binary_tree
 //
 // https://en.wikipedia.org/wiki/Search_tree
+// 二叉查找树 父节点大于左子树的所有节点，小于右子树的所有节点
+// https://en.wikipedia.org/wiki/Binary_search_tree
 // B树
 // https://en.wikipedia.org/wiki/B-tree
 // B+树
