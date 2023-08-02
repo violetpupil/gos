f414e4d7e0c606a1d86977230107e22134eff933
shouguang <495140158@qq.com>
2023-08-02 15:12:19 +0800

    二叉树

--- ald/search_tree.go
+++ ald/search_tree.go
@@ -1,4 +1,9 @@
+// 二叉树 每个节点最多有两个子节点
+// https://en.wikipedia.org/wiki/Binary_tree
+//
 // https://en.wikipedia.org/wiki/Search_tree
+// B树
 // https://en.wikipedia.org/wiki/B-tree
+// B+树
 // https://en.wikipedia.org/wiki/B+_tree
 package ald
