09febc9088e9d96a3ba753d1d0528e60bcad8490
shouguang <495140158@qq.com>
2023-08-16 11:22:38 +0800

    LoginSocks5

--- lib/imap/client.go
+++ lib/imap/client.go
@@ -3,10 +3,32 @@ package imap
 import (
 	"github.com/emersion/go-imap/client"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/net/proxy"
 )
 
+// Login 登录邮箱
 func Login(addr, username, password string) error {
 	conn, err := client.DialTLS(addr, nil)
+	if err != nil {
+		logrus.Errorln("dial tls error", err)
+		return err
+	}
+	err = conn.Login(username, password)
+	return err
+}
+
+// LoginSocks5 用socks5代理登录邮箱
+func LoginSocks5(addr, username, password, addrP, usernameP, passwordP string) error {
+	dialer, err := proxy.SOCKS5("tcp", addrP, &proxy.Auth{
+		User:     usernameP,
+		Password: passwordP,
+	}, proxy.Direct)
+	if err != nil {
+		logrus.Errorln("socks5 dialer error", err)
+		return err
+	}
+
+	conn, err := client.DialWithDialerTLS(dialer, addr, nil)
 	if err != nil {
 		logrus.Errorln("dial tls error", err)
 		return err
--- lib/imap/client_test.go
+++ lib/imap/client_test.go
@@ -9,3 +9,8 @@ func TestLogin(t *testing.T) {
 	err := Login("outlook.office365.com:993", "", "")
 	fmt.Println(err)
 }
+
+func TestLoginSocks5(t *testing.T) {
+	err := LoginSocks5("outlook.office365.com:993", "", "", "localhost:1080", "", "")
+	fmt.Println(err)
+}
