04023294b3bfb8f5d590cc7eeea4ed54753ddcee
shouguang <495140158@qq.com>
2023-08-18 14:37:20 +0800

    Value

--- lib/fastjson/fastjson.go
+++ lib/fastjson/fastjson.go
@@ -1,13 +1,23 @@
 // 无需声明，直接获取json字段值
-// https://pkg.go.dev/github.com/valyala/fastjson
 // fastjson parses the input JSON only once.
 // Validates the parsed JSON.
+// 数组使用0开始的索引
 package fastjson
 
 import "github.com/valyala/fastjson"
 
 var (
+	// 解析json字符串
+	Parse = fastjson.Parse
 	// 解析字段类型number，解析失败时，返回0
 	GetInt    = fastjson.GetInt
 	GetString = fastjson.GetString
+)
+
+type (
+	// json值
+	// Get() 获取json值，不存在返回nil
+	// GetStringBytes() 获取bytes，不存在返回nil
+	// Type() 值类型
+	Value = fastjson.Value
 )
