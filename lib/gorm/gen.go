efcc718490bce7ec0052b3bc81f39ada4f0338d0
shouguang <495140158@qq.com>
2023-08-23 11:07:03 +0800

    gentool

--- lib/gorm/gen.go
+++ lib/gorm/gen.go
@@ -6,4 +6,10 @@
 // -outPath specify a directory for output (default "./dao/query")
 // -tables specify tables want to generated from, default all tables. 逗号分隔
 // -onlyModel only generate models (without query file)
+//
+// gentool -onlyModel ^
+// -dsn "user:pwd@tcp(localhost:3306)/database?charset=utf8mb4&parseTime=True&loc=Local" ^
+// -tables "t_account,t_country,t_timezone" ^
+// -modelPkgName model ^
+// -outPath ./types/model
 package gorm
