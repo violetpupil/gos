76dcf1e4df11db2ec3235709038d228594cde6ce
shouguang <495140158@qq.com>
2023-09-07 16:07:12 +0800

    访问控制

--- lib/ali/ram/ram.go
+++ lib/ali/ram/ram.go
@@ -1,4 +1,4 @@
 // 访问控制
-// https://help.aliyun.com/product/28625.html
-// https://help.aliyun.com/document_detail/121136.html
+// https://help.aliyun.com/zh/ram/
+// https://help.aliyun.com/zh/ram/developer-reference/sts-sdk-overview
 package ram
