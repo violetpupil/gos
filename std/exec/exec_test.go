c2a12fdf00eeabecd8718230def6680b5bfaa33e
shouguang <495140158@qq.com>
2023-09-07 09:57:17 +0800

    SetFormatter

--- std/exec/exec_test.go
+++ std/exec/exec_test.go
@@ -3,9 +3,18 @@ package exec
 import (
 	"fmt"
 	"testing"
+
+	"github.com/sirupsen/logrus"
 )
 
 func TestRunCommand(t *testing.T) {
+	// 防止打印标准错误时，因引号导致换行不生效
+	logrus.SetFormatter(
+		// 等号形式 - 默认
+		&logrus.TextFormatter{
+			DisableQuote: true,
+		},
+	)
 	r, err := RunCommand("ipconfig")
 	fmt.Println(r, err)
 }
