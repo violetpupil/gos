c80e709befd7b3972306de7015660360acb85e29
shouguang <495140158@qq.com>
2023-09-11 11:19:48 +0800

    Reverse

--- spec/types/slice.go
+++ spec/types/slice.go
@@ -2,15 +2,6 @@
 // 逐个添加元素，扩容后的容量首次为2^3，之后指数递增
 package types
 
-import (
-	"sort"
-)
-
-// Reverse 反转切片
-func Reverse(x any) {
-	sort.Slice(x, func(i, j int) bool { return true })
-}
-
 // Safe 切片索引是否安全，不会导致panic
 func Safe(start, end, cap int) bool {
 	return 0 <= start && start <= end && end <= cap
--- sub/exp/slices.go
+++ sub/exp/slices.go
@@ -6,7 +6,7 @@ import "golang.org/x/exp/slices"
 func Unique(src []string) []string {
 	dst := make([]string, 0)
 	for _, s := range src {
-		if !slices.Contains[string](dst, s) {
+		if !slices.Contains[[]string, string](dst, s) {
 			dst = append(dst, s)
 		}
 	}
