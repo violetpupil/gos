60dbdbca7478abe602b2bfe4c5888d6f57a3e8b9
shouguang <495140158@qq.com>
2023-09-11 18:29:03 +0800

    Objects

--- lib/ali/oss/list.go
+++ lib/ali/oss/list.go
@@ -12,16 +12,15 @@ type (
 	// Key 对象名 目录以斜杆结尾
 	ObjectProperties = oss.ObjectProperties
 	// oss.Bucket.ListObjectsV2()结果
-	// Objects 对象列表
+	// Objects 对象列表，有时包含指定目录对象
 	// 指定oss.Delimiter("/")后, 所有后代目录及文件 -> 指定目录下文件
-	// 使用InitToken()初始化时，第一个对象一定是指定目录对象
 	// CommonPrefixes 以delimiter结尾的路径
 	// 指定oss.Delimiter("/")后, 空切片 -> 指定目录下目录路径
 	ListObjectsResultV2 = oss.ListObjectsResultV2
 )
 
 // ListObjectsAll 获取所有对象
-// root设置为true，只获取指定目录下目录和文件，分别位于结果的Objects和CommonPrefixes字段
+// root设置为true，只获取指定目录下目录和文件，分别位于结果的CommonPrefixes和Objects字段
 // root设置为false，获取所有后代目录及文件，位于结果的Objects字段
 func ListObjectsAll(prefix string, root bool) (*oss.ListObjectsResultV2, error) {
 	var out oss.ListObjectsResultV2
