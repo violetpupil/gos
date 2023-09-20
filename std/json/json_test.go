cc0c92ca5c48a9b9323905925f669f3464dc90d9
shouguang <495140158@qq.com>
2023-09-20 21:21:45 +0800

    string选项

--- std/json/json_test.go
+++ std/json/json_test.go
@@ -1,7 +1,24 @@
 package json
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
 
 func TestStdout(t *testing.T) {
 	Stdout(map[string]string{"a": "b"})
+}
+
+type Tmp struct {
+	A bool `json:",string"`
+}
+
+func TestMarshal(t *testing.T) {
+	var tmp Tmp
+	bs, err := json.Marshal(tmp)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bs))
 }
