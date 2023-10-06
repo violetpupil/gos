1498b6dbe6e8ce914f6303f0aa0738c5ffa5a844
shouguang <495140158@qq.com>
2023-10-06 14:36:53 +0800

    int类型字段 excel可能显示为科学计数法 用string类型可以避免

--- lib/exl/exl.go
+++ lib/exl/exl.go
@@ -8,6 +8,7 @@ import (
 var (
 	ReadFile = exl.ReadFile[exl.ReadConfigurator]
 	// 写到指定文件
+	// int类型字段 excel可能显示为科学计数法 用string类型可以避免
 	Write = exl.Write[exl.WriteConfigurator]
 	// 写到io.Writer
 	WriteTo = exl.WriteTo[exl.WriteConfigurator]
