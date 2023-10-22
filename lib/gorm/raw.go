948c4fa430e5f99b3b55ce2fb7f8d3faf7fb5320
shouguang <495140158@qq.com>
2023-10-22 17:10:17 +0800

    RawFirst

--- lib/gorm/raw.go
+++ lib/gorm/raw.go
@@ -18,3 +18,10 @@ func (r *raw) RawScan(sql string, dest interface{}, values ...interface{}) error
 	err := r.db.Raw(sql, values...).Scan(dest).Error
 	return err
 }
+
+// RawFirst 直接执行sql语句，并获取结果
+func (r *raw) RawFirst(sql string, dest interface{}, values ...interface{}) error {
+	// 此时 First() 不会修改语句，但会返回 gorm.ErrRecordNotFound 错误
+	err := r.db.Raw(sql, values...).First(dest).Error
+	return err
+}
