0b3747853b5679336286c8ef68e05c24afe9fbe8
shouguang <495140158@qq.com>
2023-11-07 18:23:48 +0800

    Logger

--- lib/gin/common/req.go
+++ lib/gin/common/req.go
@@ -1,11 +1,21 @@
 package common
 
+import "github.com/sirupsen/logrus"
+
 // IdReq 单个id请求
 type IdReq struct {
 	ID string `form:"id"`
 }
 
+func (r IdReq) Logger() *logrus.Entry {
+	return logrus.WithField("id", r.ID)
+}
+
 // IdsReq 多个id请求
 type IdsReq struct {
 	IDs []string `json:"ids"`
+}
+
+func (r IdsReq) Logger() *logrus.Entry {
+	return logrus.WithField("ids", r.IDs)
 }
