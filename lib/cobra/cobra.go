92dcf617def9e3b6a020ab8b2451ff389fce513c
shouguang <495140158@qq.com>
2023-11-21 18:18:28 +0800

    命令

--- lib/cobra/cobra.go
+++ lib/cobra/cobra.go
@@ -1,8 +1,13 @@
+// Commands represent actions, Args are things and Flags are modifiers for those actions.
 package cobra
 
 import "github.com/spf13/cobra"
 
 type (
 	// 命令
+	// Use one-line usage message
+	// Short short description shown in the 'help' output
+	// Long long message shown in the 'help <this-command>' output
+	// RunE 执行函数，返回错误
 	Command = cobra.Command
 )
