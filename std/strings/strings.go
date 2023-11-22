c5ed93e0c12c3755211c1d883082e84edb2ecce0
shouguang <495140158@qq.com>
2023-11-22 15:35:22 +0800

    SplitLast

--- std/strings/strings.go
+++ std/strings/strings.go
@@ -6,6 +6,8 @@ var (
 	// Split 用分隔符获取子串
 	// 如果分隔符不是空，字符串中不包含分隔符，结果是原字符串切片
 	Split = strings.Split
+	// SplitN 用分隔符获取子串，从左限定数量，数量<0相当于Split
+	SplitN = strings.SplitN
 )
 
 type (
@@ -15,3 +17,9 @@ type (
 	// WriteString() 写字符串
 	Builder = strings.Builder
 )
+
+// SplitLast 用分隔符获取最后一个子串
+func SplitLast(s, sep string) string {
+	sub := strings.Split(s, sep)
+	return sub[len(sub)-1]
+}
