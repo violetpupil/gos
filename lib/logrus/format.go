bfc575e8fdfcf23d570c20d7ed4b31f9d7685861
shouguang <495140158@qq.com>
2023-11-27 14:32:22 +0800

    json形式

--- lib/logrus/format.go
+++ lib/logrus/format.go
@@ -11,6 +11,7 @@ type (
 	// time=2023-11-27T14:22:39+08:00 level=info msg={"key": 0}
 	TextFormatter = logrus.TextFormatter
 	// json形式
+	// {"level":"info","msg":"hi","time":"2023-11-27T14:30:04+08:00"}
 	JSONFormatter = logrus.JSONFormatter
 )
 
