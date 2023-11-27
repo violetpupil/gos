311dd062cba467d418a4981c37aa3c28e7ac4cbd
shouguang <495140158@qq.com>
2023-11-27 15:52:19 +0800

    ForceColors

--- lib/logrus/format.go
+++ lib/logrus/format.go
@@ -7,9 +7,10 @@ type (
 	// time="2023-11-27T14:17:08+08:00" level=info msg=hi
 	// 空日志不显示 msg 字段
 	// 有时会添加引号 msg="{\"key\": 0}"
+	// 检测到 tty 时，会输出颜色，记录程序运行秒数 INFO[0000] hi
 	//
-	// ForceColors 带颜色输出，格式也会改变，记录程序运行秒数 INFO[0000] hi
-	// FullTimestamp 记录时间戳，而不是程序运行秒数
+	// ForceColors 强制以 tty 格式输出
+	// FullTimestamp 记录时间戳
 	// TimestampFormat 时间戳格式
 	// DisableQuote 禁用引号
 	// time=2023-11-27T14:22:39+08:00 level=info msg={"key": 0}
