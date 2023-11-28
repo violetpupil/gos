6e07e63b534027b5e2c2ec47713c6420f576dbc7
shouguang <495140158@qq.com>
2023-11-28 11:56:39 +0800

    SetSuccessResult

--- lib/req/req.go
+++ lib/req/req.go
@@ -11,9 +11,13 @@ type (
 	// 请求
 	// Get() 发起get请求
 	// SetHeaders() 设置请求头
+	// SetQueryParam() 设置查询字符串
+	// SetSuccessResult() 成功响应体
 	Request = req.Request
 	// 响应
+	// GetStatus() 获取响应码及描述
 	// GetStatusCode() 获取响应码
+	// String() 响应体字符串
 	Response = req.Response
 )
 
