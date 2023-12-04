bb44867bfa01c2869c3e36ed714b61b739f86868
shouguang <495140158@qq.com>
2023-12-04 13:49:07 +0800

    date

--- lib/sprig/sprig.go
+++ lib/sprig/sprig.go
@@ -1,4 +1,8 @@
 // 模板函数列表 https://masterminds.github.io/sprig/
+//
+// date https://masterminds.github.io/sprig/date.html
+// now The current date/time.
+// date The date function formats a date.
 package sprig
 
 import sprig "github.com/Masterminds/sprig"
