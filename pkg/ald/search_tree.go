72e5a34a7a3c40b45bf6da4e5889b06eb6996b30
shouguang <495140158@qq.com>
2023-12-07 17:29:16 +0800

    Indexes

--- pkg/ald/search_tree.go
+++ pkg/ald/search_tree.go
@@ -1,14 +1,15 @@
 // 二叉树 每个节点最多有两个子节点
 // https://en.wikipedia.org/wiki/Binary_tree
-//
 // 查找树 父节点大于左子树的所有节点，小于右子树的所有节点
 // https://en.wikipedia.org/wiki/Search_tree
+//
 // 二叉查找树
 // https://en.wikipedia.org/wiki/Binary_search_tree
-// 平衡二叉树 使二叉查找树平衡
+// 平衡二叉树 自动减小二叉查找树高度
 // https://en.wikipedia.org/wiki/Self-balancing_binary_search_tree
-// B树
+// B树 在平衡二叉树基础上，节点上可以有多个元素，子节点对应有多个
+// 因为磁盘是分块读取的，数据库索引使用B树可以减少读盘次数
 // https://en.wikipedia.org/wiki/B-tree
-// B+树
-// https://en.wikipedia.org/wiki/B+_tree
+// B+树 非叶子节点只存放索引，叶子节点存放索引和行数据
+// https://xiaolincoding.com/mysql/index/why_index_chose_bpuls_tree.html
 package ald
--- tools/mysql/index.md
+++ tools/mysql/index.md
@@ -1,1 +1,3 @@
-# [Indexes](https://dev.mysql.com/doc/refman/8.0/en/optimization-indexes.html)
+# [Indexes](https://xiaolincoding.com/mysql/index/index_interview.html)
+
+[为什么 MySQL 采用 B+ 树作为索引？](https://xiaolincoding.com/mysql/index/why_index_chose_bpuls_tree.html)
