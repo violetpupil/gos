5a2be3ee665647e37f022922ea8fe431f2c166ff
shouguang <495140158@qq.com>
2023-12-24 17:06:00 +0800

    golang面试题集合

--- misc/job/job.md
+++ misc/job/job.md
@@ -1,3 +1,5 @@
 # job
 
 [smartx](https://www.smartx.com/jobs/)
+
+[golang面试题集合](https://github.com/lifei6671/interview-go)
---
+++ misc/job/program.go
@@ -0,0 +1,5 @@
+package job
+
+// 交替打印数字和字母
+// https://github.com/lifei6671/interview-go/blob/master/question/q001.md
+func PrintNumberAlphabet() {}
