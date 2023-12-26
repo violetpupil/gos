4411fffd56596ee925387d40044813ff4b150d24
shouguang <495140158@qq.com>
2023-12-26 16:50:39 +0800

    captcha

---
+++ lib/ali/captcha/captcha.go
@@ -0,0 +1,3 @@
+// 交互流程 https://help.aliyun.com/zh/captcha/captcha2-0/product-overview/what-is-alibaba-cloud-captcha-2
+// 接入流程 https://help.aliyun.com/zh/captcha/captcha2-0/user-guide/access-guidelines
+package captcha
