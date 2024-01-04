ac897e971e5fb6c9b7d951dcf66803c9ecaa0057
shouguang <495140158@qq.com>
2024-01-04 12:51:52 +0800

    PrintNumberAlphabet

--- misc/job/program.go
+++ misc/job/program.go
@@ -1,5 +1,41 @@
 package job
 
+import (
+	"fmt"
+)
+
 // 交替打印数字和字母
 // https://github.com/lifei6671/interview-go/blob/master/question/q001.md
-func PrintNumberAlphabet() {}
+func PrintNumberAlphabet() {
+	letter, number := make(chan bool), make(chan bool)
+	exit := make(chan bool)
+
+	go func() {
+		i := 1
+		for range number {
+			fmt.Print(i)
+			i++
+			fmt.Print(i)
+			i++
+			letter <- true
+		}
+	}()
+
+	go func() {
+		i := 'A'
+		for range letter {
+			if i >= 'Z' {
+				exit <- true
+				return
+			}
+
+			fmt.Print(string(i))
+			i++
+			fmt.Print(string(i))
+			i++
+			number <- true
+		}
+	}()
+	number <- true
+	<-exit
+}
---
+++ misc/job/program_test.go
@@ -0,0 +1,7 @@
+package job
+
+import "testing"
+
+func TestPrintNumberAlphabet(t *testing.T) {
+	PrintNumberAlphabet()
+}
