791e7598a8b7d57d11f228c22b8493884cb3c454
shouguang <495140158@qq.com>
2024-02-23 17:46:16 +0800

    ExchangeSingle

---
+++ lib/ali/api/api.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"github.com/go-resty/resty/v2"
+)
+
+type ExchangeSingleRes struct {
+	Result struct {
+		List map[string]Exchange `json:"list"`
+	} `json:"result"`
+}
+
+type Exchange struct {
+	Name       string `json:"name"`
+	Rate       string `json:"rate"`
+	UpdateTime string `json:"updatetime"`
+}
+
+// ExchangeSingle 单个货币汇率查询
+func ExchangeSingle(appCode string) (ExchangeSingleRes, error) {
+	var result ExchangeSingleRes
+	_, err := resty.New().R().
+		SetHeader("Authorization", "APPCODE "+appCode).
+		SetQueryParam("currency", "USD").
+		SetResult(&result).
+		Get("https://jisuhuilv.market.alicloudapi.com/exchange/single")
+	return result, err
+}
