0fdf4fefe8000a095fab5f3d284f2d1ceaa0aff2
shouguang <495140158@qq.com>
2024-08-06 10:07:12 +0800

    加锁示例

--- lib/redsync/redsync.go
+++ lib/redsync/redsync.go
@@ -3,9 +3,12 @@
 package redsync
 
 import (
+	"time"
+
 	"github.com/go-redsync/redsync/v4"
 	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
 	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
 )
 
 // New 创建redis分布式锁管理
@@ -13,4 +16,31 @@ func New(client *redis.Client) *redsync.Redsync {
 	pool := goredis.NewPool(client)
 	m := redsync.New(pool)
 	return m
+}
+
+// 加锁示例
+func Lock() error {
+	// prepare
+	var sync *redsync.Redsync
+	key := ""
+
+	lock := sync.NewMutex(
+		key,
+		redsync.WithExpiry(time.Minute),
+		redsync.WithTries(1000),
+	)
+	err := lock.Lock()
+	if err != nil {
+		zap.L().Error("lock error", zap.Error(err))
+		return err
+	} else {
+		zap.L().Info("lock success")
+	}
+	defer func() {
+		r, err := lock.Unlock()
+		zap.L().Info("unlock result", zap.Bool("status", r), zap.Error(err))
+	}()
+
+	// do something
+	return nil
 }
