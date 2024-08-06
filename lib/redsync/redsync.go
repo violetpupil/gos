72fc4b8a93c7c5d0fce4d8b9d997f68dbe01e923
shouguang <495140158@qq.com>
2024-08-06 10:01:27 +0800

    clear

--- lib/redsync/redsync.go
+++ lib/redsync/redsync.go
@@ -3,40 +3,14 @@
 package redsync
 
 import (
-	"time"
-
 	"github.com/go-redsync/redsync/v4"
 	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
 	"github.com/redis/go-redis/v9"
 )
-
-type (
-	Redsync = redsync.Redsync
-)
-
-var (
-	WithExpiry = redsync.WithExpiry
-)
-
-// m redis分布式锁管理
-// 必须先调用Init初始化
-var m *redsync.Redsync
-
-// Init 初始化锁管理
-func Init(client *redis.Client) {
-	pool := goredis.NewPool(client)
-	m = redsync.New(pool)
-}
 
 // New 创建redis分布式锁管理
 func New(client *redis.Client) *redsync.Redsync {
 	pool := goredis.NewPool(client)
 	m := redsync.New(pool)
 	return m
-}
-
-// NewMutex returns a new distributed mutex with given name.
-// 使用redsync.Mutex.Lock()和redsync.Mutex.Unlock()加解锁
-func NewMutex(name string, expiry time.Duration) *redsync.Mutex {
-	return m.NewMutex(name, redsync.WithExpiry(expiry))
 }
