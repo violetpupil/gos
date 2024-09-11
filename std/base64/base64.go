adfb59e850feb2acda7e59e0851233ea8b8482a0
shouguang <495140158@qq.com>
2024-09-11 22:39:40 +0800

    base64

--- std/base64/base64.go
+++ std/base64/base64.go
@@ -1,21 +1,4 @@
 package base64
 
-import "encoding/base64"
-
 // Encode 每6个bit位编码一个字节，字节数会增大三分之一
-func Encode(src []byte) string {
-	return base64.StdEncoding.EncodeToString(src)
-}
-
-func Decode(s string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(s)
-}
-
 // URLEncode 可直接用在url中的base64编码字符串
-func URLEncode(src []byte) string {
-	return base64.URLEncoding.EncodeToString(src)
-}
-
-func URLDecode(s string) ([]byte, error) {
-	return base64.URLEncoding.DecodeString(s)
-}
---
+++ utils/base64.md
@@ -0,0 +1,8 @@
+# base64
+
+```golang
+base64.StdEncoding.EncodeToString
+base64.StdEncoding.DecodeString
+base64.URLEncoding.EncodeToString
+base64.URLEncoding.DecodeString
+```
