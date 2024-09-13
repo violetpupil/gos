2a17fc722bd132b9054abd818da3af139b239255
shouguang <495140158@qq.com>
2024-09-13 15:56:51 +0800

    JSONLoad

--- std/json/json.go
+++ std/json/json.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/sirupsen/logrus"
 	"go.uber.org/zap"
 )
 
@@ -22,29 +21,6 @@ var (
 	// nil for JSON null
 	Unmarshal = json.Unmarshal
 )
-
-// Load 加载json文件，v是非nil指针
-func Load(name string, v any) error {
-	bs, err := os.ReadFile(name)
-	if err != nil {
-		logrus.Error("read file error ", err)
-		return err
-	}
-	err = json.Unmarshal(bs, v)
-	return err
-}
-
-// Dump 写入json文件，并格式化
-func Dump(v any, name string) error {
-	// 缩进两个空格
-	bs, err := json.MarshalIndent(v, "", "  ")
-	if err != nil {
-		logrus.Error("json marshal error ", err)
-		return err
-	}
-	err = os.WriteFile(name, bs, 0666)
-	return err
-}
 
 // MarshalIndent json编码并缩进
 func MarshalIndent(v any) ([]byte, error) {
---
+++ utils/json.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func JSONLoad(name string, v any) error {
+	bs, err := os.ReadFile(name)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bs, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func JSONDump(name string, v any) error {
+	bs, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(name, bs, 0666)
+	if err != nil {
+		return err
+	}
+	return nil
+}
