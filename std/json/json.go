7ead9e31b962080471fb570ee0dbeab9efc9f607
shouguang <495140158@qq.com>
2024-09-13 16:05:45 +0800

    clear

--- std/json/json.go
+++ std/json/json.go
@@ -9,11 +9,7 @@ var (
 	// 结构体使用指针字段，可以区分不存在还是赋了默认值
 	//
 	// 当json解码到any时，接口实际类型对应关系
-	// bool, for JSON booleans
-	// float64, for JSON numbers
-	// string, for JSON strings
 	// []interface{}, for JSON arrays
 	// map[string]interface{}, for JSON objects
-	// nil for JSON null
 	Unmarshal = json.Unmarshal
 )
