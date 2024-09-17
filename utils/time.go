3bccae2b8c689a5e411db2236472ac50b887a1fe
instafever <495140158@qq.com>
2024-09-18 01:03:28 +0800

    TodayLeft

--- utils/time.go
+++ utils/time.go
@@ -5,3 +5,10 @@ import "time"
 func SetTimeZone(t time.Time, offset float32) time.Time {
 	return t.In(time.FixedZone("", int(offset*3600)))
 }
+
+// 0时区今天剩余时间
+func TodayLeft() time.Duration {
+	now := time.Now().UTC()
+	tomorrow := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).AddDate(0, 0, 1)
+	return tomorrow.Sub(now)
+}
---
+++ utils/time_test.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestTodayLeft(t *testing.T) {
+	t.Log(TodayLeft())
+}
