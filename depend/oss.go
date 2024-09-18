b214f1be3b943c4116f4f0eab33a5e8ace1658c6
shouguang <495140158@qq.com>
2024-09-18 14:18:10 +0800

    NewBucket

---
+++ depend/oss.go
@@ -0,0 +1,20 @@
+package depend
+
+import (
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"go.uber.org/zap"
+)
+
+func NewBucket(endpoint, accessKeyID, accessKeySecret, bucketName string) (*oss.Bucket, error) {
+	c, err := oss.New(endpoint, accessKeyID, accessKeySecret)
+	if err != nil {
+		zap.L().Error("new client error", zap.Error(err))
+		return nil, err
+	}
+	b, err := c.Bucket(bucketName)
+	if err != nil {
+		zap.L().Error("get bucket error", zap.Error(err))
+		return nil, err
+	}
+	return b, nil
+}
