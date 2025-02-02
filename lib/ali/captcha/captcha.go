6d4fa44e044d0cf69e2cda2c3d097c258f052449
shouguang <495140158@qq.com>
2025-02-02 22:47:09 +0800

    clear

--- lib/ali/captcha/captcha.go
+++
@@ -1,3 +0,0 @@
-// 交互流程 https://help.aliyun.com/zh/captcha/captcha2-0/product-overview/what-is-alibaba-cloud-captcha-2
-// 接入流程 https://help.aliyun.com/zh/captcha/captcha2-0/user-guide/access-guidelines
-package captcha
