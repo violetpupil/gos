38d058a24b34a009d4bc709b9ad7140dc2867b1d
shouguang <495140158@qq.com>
2025-02-19 19:39:54 +0800

    clear

--- pkg/types/slice.go
+++ pkg/types/slice.go
@@ -7,19 +7,6 @@ func Safe(start, end, cap int) bool {
 	return 0 <= start && start <= end && end <= cap
 }
 
-// Div 将切片按顺序转为二维切片
-func Div[T any](src []T, size int) [][]T {
-	dst := make([][]T, 0)
-	for i := 0; i < len(src); i += size {
-		if i+size >= len(src) {
-			dst = append(dst, src[i:])
-		} else {
-			dst = append(dst, src[i:i+size])
-		}
-	}
-	return dst
-}
-
 // DeepCopy 深拷贝切片
 func DeepCopy[Type any](src []Type) []Type {
 	dst := make([]Type, len(src))
