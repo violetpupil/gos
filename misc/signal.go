118285fa1698732c35745902cb8f3d2ae23db6a1
shouguang <495140158@qq.com>
2025-05-09 15:31:17 +0800

    clear

---
+++ misc/signal.go
@@ -0,0 +1,14 @@
+package misc
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// GracefullyExit 阻塞等待 INT TERM 信号
+func GracefullyExit() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+}
