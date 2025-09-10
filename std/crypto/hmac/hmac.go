53632a1d4b39ba948514f8395b890ead39aaf3c8
shouguang <495140158@qq.com>
2025-09-10 09:59:10 +0800

    clear

--- std/crypto/hmac/hmac.go
+++ std/crypto/hmac/hmac.go
@@ -3,7 +3,6 @@ package hmac
 import (
 	"crypto/hmac"
 	"crypto/sha1"
-	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
 	"hash"
@@ -30,19 +29,4 @@ func HmacSha1Hex(key, s string) string {
 // HmacSha1Base64 hmac消息认证码sha1 base64摘要
 func HmacSha1Base64(key, s string) string {
 	return base64.StdEncoding.EncodeToString(HmacSha1(key, s))
-}
-
-// HmacSha256 hmac消息认证码sha256哈希摘要
-func HmacSha256(key string, s string) []byte {
-	return Sum(sha256.New, key, s)
-}
-
-// HmacSha1Hex hmac消息认证码sha256 16进制摘要
-func HmacSha256Hex(key, s string) string {
-	return fmt.Sprintf("%x", Sum(sha256.New, key, s))
-}
-
-// HmacSha256Base64 hmac消息认证码sha256 base64摘要
-func HmacSha256Base64(key, s string) string {
-	return base64.StdEncoding.EncodeToString(HmacSha256(key, s))
 }
