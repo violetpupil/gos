bad876d6185187163c308bf39ad0f3353b5e265c
shouguang <495140158@qq.com>
2026-01-09 11:11:32 +0800

    NewMysqlDB

--- depend/gorm/gorm.go
+++ depend/gorm/gorm.go
@@ -1,1 +1,36 @@
 package gorm
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Pass     string
+	Database string
+}
+
+func NewMysqlDialector(c Config) gorm.Dialector {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.User, c.Pass, c.Host, c.Port, c.Database,
+	)
+	config := mysql.Config{
+		DSN: dsn,
+	}
+	dialector := mysql.New(config)
+	return dialector
+}
+
+func NewMysqlDB(c Config) (*gorm.DB, error) {
+	dialector := NewMysqlDialector(c)
+
+	opt := &gorm.Config{}
+	db, err := gorm.Open(dialector, opt)
+	return db, err
+}
